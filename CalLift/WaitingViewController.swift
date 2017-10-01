03828f0ef22a3e0aa290f1660e09448a5c94ac3d
Leon Mak <leonmakansheng@gmail.com>
2017-10-01 11:35:43 -0700

    Add lift animation

--- CalLift/WaitingViewController.swift
+++ CalLift/WaitingViewController.swift
@@ -10,21 +10,66 @@ import UIKit
 
 class WaitingViewController: UIViewController {
 
+    @IBOutlet weak var liftSquareView: UIView!
+    @IBOutlet weak var backgroundView: UIView!
+
+    @IBOutlet weak var timeLeftLbl: UILabel!
     // Passed by delegate
     var assignedLift: KoneLift!
     var timer: Timer?
+    
+    // Mock
+    var maxLevel = KoneManager.instance.floors.count
+    var maxHeight = 0
 
+    @IBAction func backBtnPressed(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setupBgView()
+        setupLiftView()
     }
-
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    func setupLiftView() {
+        liftSquareView.setBlur(style: .light, corner: 6.0, replaceViewAlpha: true)
+    }
+    
+    func animateToFloor(floor: Int) {
+        UIView.animate(withDuration: 1.0, delay: 1.0, options: .curveEaseInOut, animations: {
+            self.liftSquareView.frame = CGRect(x: self.liftSquareView.frame.origin.x,
+                                         y: self.backgroundView.frame.height * CGFloat(1 - (floor/self.maxLevel)),
+                                         width: self.liftSquareView.frame.width,
+                                         height: self.liftSquareView.frame.height)
+        }, completion: nil)
+    }
+    
+    func setupBgView() {
+        let imageView = UIImageView(image: #imageLiteral(resourceName: "plug-and-play-silicon-valley"))
+        imageView.contentMode = .scaleAspectFill
+        imageView.bounds = view.bounds
+        imageView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height)
+        imageView.layer.zPosition = -10
+        imageView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        imageView.setBlur(style: .dark, alpha: 0.75)
+        self.view.addSubview(imageView)
+    }
+    
     func startPollingForLiftUpdates() {
-        timer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true) { [unowned self] _ in
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { [unowned self] _ in
             KoneManager.instance.getLiftState(liftId: self.assignedLift.id) { state in
-                print(state)
+                let floorIdx = KoneManager.instance.floors.index(where: {floor in
+                    return floor.typicalLevel == state.level
+                })
+                self.animateToFloor(floor: floorIdx!)
             }
+            
         }
     }
 
