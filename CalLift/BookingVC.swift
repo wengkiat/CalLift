73a6cc0f5e497e8dad3b3853e1f41ee885cd1de9
Leon Mak <leonmakansheng@gmail.com>
2017-10-01 03:41:54 -0700

    Add ui

--- CalLift/BookingVC.swift
+++ CalLift/BookingVC.swift
@@ -10,14 +10,95 @@ import UIKit
 
 class BookingVC: UIViewController {
 
+    @IBOutlet weak var nextEventView: UIView!
+    @IBOutlet weak var nextEventLbl: UILabel!
+    @IBOutlet weak var nextEventLocation: UILabel!
+    @IBOutlet weak var nextEventStartScanLbl: UILabel!
+    @IBOutlet weak var nextEventCountdownLbl: UILabel!
+    
+    @IBOutlet weak var sourceView: UIView!
+    @IBOutlet weak var sourceDescription: UILabel!
+    @IBOutlet weak var sourceLocation: UILabel!
+    
+    @IBOutlet weak var destinationView: UIView!
+    @IBOutlet weak var destinationDescription: UILabel!
+    @IBOutlet weak var destinationLocation: UILabel!
+    
+    var callNowBtn: ColoredButton?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         KoneManager.instance.getDestinations(completion: showDestinations)
+        
+        setupBgView()
+        setupEventViews()
+        
+        displayCallNowBtn()
     }
     
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    // MARK: - Setup
     @objc func showDestinations(_ dict: [String: Any]) {
         print(dict.getItems())
+    }
+    
+    func setupBgView() {
+        let imageView = UIImageView(image: #imageLiteral(resourceName: "plug-and-play-silicon-valley"))
+        imageView.contentMode = .scaleAspectFill
+        imageView.bounds = view.bounds
+        imageView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height)
+        imageView.layer.zPosition = -10
+        imageView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        imageView.setBlur(style: .dark, alpha: 0.75)
+        self.view.addSubview(imageView)
+    }
+    
+    func setupEventViews() {
+        let radius = CGFloat(6.0)
+        
+        self.nextEventView.setBlur(style: .light, alpha: 0.5, replaceViewAlpha: true)
+        self.sourceView.setBlur(style: .dark, alpha: 0.5, replaceViewAlpha: true)
+        self.destinationView.setBlur(style: .dark, alpha: 0.5, replaceViewAlpha: true)
+        
+        self.sourceView.setCornerRadius(radius: radius)
+        self.nextEventView.setCornerRadius(radius: radius)
+        self.destinationView.setCornerRadius(radius: radius)
+        
+        self.sourceView.setShadow()
+        self.nextEventView.setShadow()
+        self.destinationView.setShadow()
+        
+        
+    }
+
+    func displayCallNowBtn() {
+        if self.callNowBtn != nil {
+            return
+        }
+        let width = view.frame.width * 0.8
+        let height = CGFloat(45.0)
+        let x = view.frame.width * 0.1
+        let y = view.frame.height * 0.9
+        let frame = CGRect(x: x, y: y, width: width, height: height)
+        self.callNowBtn = ColoredButton(frame: frame,
+                                         color: UIColor.clear,
+                                         borderColor: UIColor.clear)
+        self.callNowBtn!.frame = frame
+        self.callNowBtn!.setTitle("Call Now", for: .normal)
+        self.callNowBtn!.addTarget(self, action: #selector(self.callNowBtnTouched(_:)), for: .touchUpInside)
+        self.callNowBtn!.setBlur(style: .light)
+        self.callNowBtn?.setTitleColor(UIColor.white, for: .normal)
+        self.view.addSubview(self.callNowBtn!)
+    }
+    
+    @objc func callNowBtnTouched(_ sender: AnyObject?) {
+        if sender === self.callNowBtn {
+            print("CALL LIFT NOW")
+        }
     }
     
 
--- CalLift/Constants.swift
+++ CalLift/Constants.swift
@@ -7,7 +7,7 @@
 //
 
 import CoreBluetooth
-import Foundation
+import UIKit
 
 struct Constants {
     
@@ -27,6 +27,12 @@ struct Constants {
         static let clientId = "711eff77-74cc-448d-a4b3-42f30e010502"
         static let secretKey = "A4hY4oN2qP5yL7uN0uS5hV6kK3vD0tE0nS8uD5qL7wC0uQ3iR2"
         static let buildingId = 9990000508
+    }
+    
+    struct Colors {
+        static let flatGray = UIColor(red: 149/255.0, green: 165/255.0, blue: 166/255.0, alpha: 1.0)
+        static let flatDarkGreen = UIColor(red: 39/255.0, green: 174/255.0, blue: 96/255.0, alpha: 1.0)
+        static let flatGreen = UIColor(red: 46/255.0, green: 204/255.0, blue: 113/255.0, alpha: 1.0)
     }
 
 }
