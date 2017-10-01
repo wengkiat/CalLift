05ca7a36d35e6acfb304ecb5bb0fe5fcf5b46055
Edmund Mok <edmundmok@outlook.com>
2017-09-30 19:23:56 -0700

    Add LiftCalendar

---
+++ CalLift/Calendar/LiftCalendar.swift
@@ -0,0 +1,31 @@
+//
+//  LiftCalendar.swift
+//  CalLift
+//
+//  Created by Edmund Mok on 9/30/17.
+//  Copyright © 2017 Edmund Mok. All rights reserved.
+//
+
+import EventKit
+import Foundation
+
+class LiftCalendar {
+    
+    var calendar: EKCalendar!
+
+    func getUpcomingEvents() -> [EKEvent] {
+        let startDate = Date()
+        let endDate = Calendar.current.date(
+            byAdding: .month, value: Constants.LiftCalendar.endMonths, to: startDate)!
+        return getEvents(from: startDate, to: endDate)
+    }
+
+    func getEvents(from startDate: Date, to endDate: Date) -> [EKEvent] {
+        let eventStore = EKEventStore()
+        let eventsPredicate = eventStore.predicateForEvents(withStart: startDate, end: endDate, calendars: [calendar])
+        return eventStore.events(matching: eventsPredicate).sorted { (e1, e2) -> Bool in
+            return e1.startDate.compare(e2.startDate) == ComparisonResult.orderedAscending
+        }
+    }
+    
+}
---
+++ CalLift/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  CalLift
+//
+//  Created by Edmund Mok on 9/30/17.
+//  Copyright © 2017 Edmund Mok. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+
+    struct LiftCalendar {
+        static let endMonths = 1
+    }
+
+}
