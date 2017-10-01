8f9a32e5e75b890cfba1558c4f7d6051ebd989a5
Edmund Mok <edmundmok@outlook.com>
2017-10-01 10:03:17 -0700

    Create waitingvc

---
+++ CalLift/WaitingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  WaitingViewController.swift
+//  CalLift
+//
+//  Created by Edmund Mok on 10/1/17.
+//  Copyright © 2017 Edmund Mok. All rights reserved.
+//
+
+import UIKit
+
+class WaitingViewController: UIViewController {
+
+    // Passed by delegate
+    var assignedLift: KoneLift!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    func startPollingForLiftUpdates() {
+        
+    }
+
+    func stopPollingForLiftUpdates() {
+
+    }
+
+}
